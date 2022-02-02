02a6572a7f3357bd25df65b59a68eeccb17b1109
Guy Morag <xpronet4@gmail.com>
2022-02-02 22:23:24 +0200

    changed vgg11.py to vgg11_bn.py

--- models/vgg11_bn.py
+++ models/vgg11_bn.py
@@ -6,27 +6,26 @@ from torch.utils.data import DataLoader
 import torchvision.models as models
 
 
-class VGG11(nn.Module):
+class VGG11bn(nn.Module):
     def __init__(self):
         super().__init__()
 
         # get original model
-        vgg11 = models.vgg11()
+        vgg11_bn = models.vgg11_bn()
 
         # modify input channels
-        input_layer = vgg11.features[0]
-        vgg11.features[0] = nn.Conv2d(1,
-                                      input_layer.out_channels,
-                                      kernel_size=input_layer.kernel_size,
-                                      stride=input_layer.stride,
-                                      padding=input_layer.padding,
-                                      bias=input_layer.bias)
+        input_layer = vgg11_bn.features[0]
+        vgg11_bn.features[0] = nn.Conv2d(1,
+                                         input_layer.out_channels,
+                                         kernel_size=input_layer.kernel_size,
+                                         stride=input_layer.stride,
+                                         padding=input_layer.padding)
 
         # modify last layer to match output classes
-        num_features = vgg11.classifier[-1].in_features
-        vgg11.classifier[-1] = nn.Linear(num_features, 10)
+        num_features = vgg11_bn.classifier[-1].in_features
+        vgg11_bn.classifier[-1] = nn.Linear(num_features, 10)
 
-        self.model = vgg11
+        self.model = vgg11_bn
 
     def forward(self, x):
         output = self.model(x)
@@ -35,7 +34,7 @@ class VGG11(nn.Module):
 
 def main():
     ds = BlockStylePVR(train=True, size=10)
-    net = VGG11()
+    net = VGG11bn()
     dl = DataLoader(ds, batch_size=2)
 
     for sample, value in dl:
