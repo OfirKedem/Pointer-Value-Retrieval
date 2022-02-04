d8d83a5e8f5b90581b68004a592afc505f1f2fc5
Guy Morag <xpronet4@gmail.com>
2022-02-04 17:38:26 +0200

    log_step changed to progress bar only

--- callbacks/logger_callback.py
+++ callbacks/logger_callback.py
@@ -1,6 +1,12 @@
 from pytorch_lightning.callbacks import Callback
+import wandb
 
 
 class LoggerCallback(Callback):
+    def __init__(self, ):
+        super().__init__()
+
     def on_batch_end(self, trainer: "pl.Trainer", pl_module: "pl.LightningModule") -> None:
-        pl_module.log("log_step", (pl_module.global_step + 1) / trainer.log_every_n_steps, prog_bar=True)
+        # log_step is the fraction of 'Step' the appears in wandb
+        # log only to progress bar
+        pl_module.log("log_step", (pl_module.global_step + 1) / trainer.log_every_n_steps, prog_bar=True, logger=False)
