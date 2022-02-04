9f20bcd065972da810cd2ddbcdd4d81886310427
Guy Morag <xpronet4@gmail.com>
2022-02-04 23:42:29 +0200

    rename log_step to f_step

--- callbacks/logger_callback.py
+++ callbacks/logger_callback.py
@@ -7,6 +7,6 @@ class LoggerCallback(Callback):
         super().__init__()
 
     def on_batch_end(self, trainer: "pl.Trainer", pl_module: "pl.LightningModule") -> None:
-        # log_step is the fraction of 'Step' the appears in wandb
+        # f_step is the fraction of 'Step' the appears in wandb
         # log only to progress bar
-        pl_module.log("log_step", (pl_module.global_step + 1) / trainer.log_every_n_steps, prog_bar=True, logger=False)
+        pl_module.log("f_step", (pl_module.global_step + 1) / trainer.log_every_n_steps, prog_bar=True, logger=False)
