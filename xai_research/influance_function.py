cb96474c6341d2de18d808480419116b6e8e3336
ofirkedem <ofirk101@gmail.com>
2022-02-12 21:01:04 +0200

    improve influence

--- xai_research/influance_function.py
+++ xai_research/influance_function.py
@@ -1,5 +1,6 @@
 import os
 import yaml
+import sys
 from argparse import ArgumentParser
 from typing import Union
 from pathlib import Path
@@ -8,10 +9,12 @@ import wandb
 import torch
 import pytorch_influence_functions as ptif
 
+sys.path.append("..")
+
 from models import MLP
 from train import setup_loaders
 
-WANDB_PATH = Path("../wandb")
+WANDB_PATH = Path("wandb")
 
 
 def remove_model_from_keys(state_dict: dict):
@@ -74,11 +77,13 @@ def get_run(run_id):
     return run_path
 
 
-def main(run_id: str, epoch: int, use_cuda=False):
+def compute_influence(run_id: str, epoch: int, num_test_points: int = 1, use_cuda=False):
     """
 
     Args:
         run_id: WandB run ID
+        epoch:
+        num_test_points:
         use_cuda:
     """
     run_dir = get_run(run_id)
@@ -95,7 +100,6 @@ def main(run_id: str, epoch: int, use_cuda=False):
     if use_cuda:
         model.cuda()
 
-    config['data']['train_params']['size'] = 20
     trainloader, testloader = setup_loaders(config)
 
     ptif.init_logging()
@@ -105,23 +109,24 @@ def main(run_id: str, epoch: int, use_cuda=False):
     if_config['gpu'] = 1 if use_cuda else -1
     if_config['dataset'] = config['data']['task']
     if_config['num_classes'] = 1
-    if_config['test_sample_num'] = 1
+    if_config['test_sample_num'] = num_test_points
 
-    influences = ptif.calc_img_wise(if_config, model, trainloader, testloader)
-    pass
+    ptif.calc_img_wise(if_config, model, trainloader, testloader)
 
-
-if __name__ == "__main__":
-    parser = ArgumentParser()
-    parser.add_argument("-r", "--run", type=str, default='m21q11cw',
-                        help="run identifier in WandB")
-
-    parser.add_argument("-e", "--epoch", type=int, default=75,
-                        help="epoch checkpoint")
-
-    parser.add_argument("-c", "--cuda", action='store_true',
-                        help="run on GPU or not")
-
-    args = parser.parse_args()
-
-    main(run_id=args.run, epoch=args.epoch, use_cuda=args.cuda)
+#
+# if __name__ == "__main__":
+#     parser = ArgumentParser()
+#     parser.add_argument("-r", "--run", type=str, default='m21q11cw',
+#                         help="run identifier in WandB")
+#
+#     parser.add_argument("-e", "--epoch", type=int, default=75,
+#                         help="epoch checkpoint")
+#
+#     parser.add_argument("-c", "--cuda", action='store_true',
+#                         help="run on GPU or not")
+#
+#     parser.add_argument("-n", type=int, default=1, help="number of test points to run on.")
+#
+#     args = parser.parse_args()
+#
+#     compute_influence(run_id=args.run, epoch=args.epoch, num_test_points=args.n, use_cuda=args.cuda)
